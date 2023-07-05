87993cc45ae0dc2bf0ccea65805057027e14b34a
Victor Ogunyemi <ogunyemivictor738@gmail.com>
2023-07-05 23:13:55 +0100

    Add files via upload

---
+++ london_bikes.py
@@ -0,0 +1,18 @@
+
+#pip install pandas
+#pip install zipfile
+#pip install kaggle
+
+
+
+
+
+#import the pandas library
+import pandas as pd
+
+#import zipfile (to extract the data downloaded from kaggle)
+import zipfile
+
+#To download the dataset programmtically from kaggle
+import kaggle
+
